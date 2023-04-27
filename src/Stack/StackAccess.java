665819c25dbf0a0474ae14fd4a8295549a84cf87
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2023-04-27 21:45:07 +0500

    Cleared Extra Files

--- src/Stack/StackAccess.java
+++ src/Stack/StackAccess.java
@@ -7,7 +7,9 @@ public class StackAccess {
         stack1.push(3);
         stack1.push(4);
         stack1.display();
+        System.out.println();
         stack1.pop();
-        stack1.display();
+        System.out.println(stack1.peek());
+//        stack1.display();
     }
 }
